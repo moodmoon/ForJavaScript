f43a442ff807acb29d6d09c60faac690a46546fb
pearmai <111360164+moodmoon@users.noreply.github.com>
2024-02-15 00:46:21 +0700

    Update todo.js

--- practice/hw3/1/todo.js
+++ practice/hw3/1/todo.js
@@ -1,15 +1,40 @@
 function init() {
-  const addButtonElement = document.getElementById('addButton');
-  addButtonElement.addEventListener('click', addTask);
+  //your code here
+  const btn = document.getElementById('addButton');
+  btn.addEventListener('click', addTask);
+
+  const rmbtn = document.getElementById('rmButton');
+  rmbtn.addEventListener('click', removebtn);
+
+  const rmbtnAll = document.getElementById('rmButtonAll');
+  rmbtnAll.addEventListener('click', removebtnAll);
 }
 function addTask() {
-  const newLiElement = document.createElement('li');
-  const inputElement = document.getElementById('todoInput');
-  const ulElement = document.getElementById('todoList');
+  //your code here
+  const inp = document.getElementById('todoInput');
+  const ulEle = document.getElementById('todoList');
+  const newli = document.createElement('li');
+  if (!inp.value) {
+    return (newli.textContent = '');
+  } else {
+    newli.textContent = inp.value;
+  }
+  inp.value = '';
+  ulEle.appendChild(newli);
+}
 
-  newLiElement.textContent = inputElement.value;
-  ulElement.appendChild(newLiElement);
-  inputElement.value = '';
+function removebtn() {
+  const ulEle = document.getElementById('todoList');
+  ulEle.lastElementChild.remove();
+}
+function removebtnAll() {
+  const ulEle = document.getElementById('todoList');
+  Array.from(ulEle.children).forEach((element) => {
+    element.remove();
+    console.log(ulEle.children);
+  });
+  // ulEle.textContent = ''
 }
 
 init();
+
